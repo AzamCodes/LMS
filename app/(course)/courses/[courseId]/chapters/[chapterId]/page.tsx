618031d0a95c490e1b0556010cf3132af45dcd3c
AzamCodes <azam.shaikh9105@gmail.com>
2024-11-19 15:09:45 +0530

    price

--- app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -79,7 +79,7 @@ const ChapterIdPage = async ({
             ) : (
               <CourseEnrollButton
                 courseId={params.courseId}
-                price={course.price}
+                price={course.price!}
               />
             )}
           </div>
